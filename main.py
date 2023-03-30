dc7d75039e60f0ded0fc9f3644226efe5e7837d6
infraestructura531 <infraestructura@telam.com.ar>
2023-03-30 17:38:13 -0300

    Add function scraper dollar, and add column in csv file with value represented in dollars | by jpgladich

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from geopy.geocoders import Nominatim
 
 URL = "https://listado.mercadolibre.com.ar/klipsch"
 
-DOLAR_BLUE_HOY = float(245.0)
+DOLAR_BLUE_HOY = "https://dolarhoy.com/cotizaciondolarblue"
 items_dict = []
 
 
@@ -18,8 +18,29 @@ def dict_to_csv_file(listate: list, csv_file: str) -> None:
     except IOError:
         print("I/O error")
 
+def dolar_blue() -> float:
+    response = requests.get(DOLAR_BLUE_HOY)
+    soup = BeautifulSoup(response.text, "html.parser")
+    soup.find(class_="tile is-parent is-8").find(class_="tile is-child").text
+    last_price_usd = None  # Initialize to None in case no price is found
+    for item in soup.findAll(class_='tile is-child'):
+        try:
+            price_usd = item.find(class_='value').text
+            price_usd = str(price_usd).replace('$', '')
+            last_price_usd = price_usd  # Update last_price_usd with the latest value
+
+        except AttributeError:
+            pass
+
+    if last_price_usd is not None:  # Check if a price was found
+        print("Sale price " + last_price_usd)
+        return float(last_price_usd)
+    else:
+        print("No price found")
+        return 0.0  # Return a default value if no price was found
 
 def main():
+    sale = str(dolar_blue())
     page = 1
     response = requests.get(URL)
     soup = BeautifulSoup(response.text, "html.parser")
@@ -31,9 +52,12 @@ def main():
             price = item.find(class_='price-tag-fraction').text.replace('.', '')
             title = item.find(class_='ui-search-item__title shops__item-title')
             link = item.find(class_='ui-search-link').get('href')
+            result_price = round(float(price) / float(sale), 2)
+
             items_dict.append(
                 {
                     'title': title.text.replace(',', ''),
+                    'price_usd': str(result_price),
                     'price': str(price),
                     'link': link,
                 }
